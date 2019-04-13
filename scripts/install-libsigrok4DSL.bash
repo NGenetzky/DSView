7f73c18e40519e2f8d03a31382c03376e798877b
Nathan Genetzky <nathan@genetzky.us>
2019-04-13 17:07:31 -0500

    scripts: Scripts to build and install libraries

---
+++ scripts/install-libsigrok4DSL.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+SCRIPTDIR="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)"
+LIBNAME='libsigrok4DSL'
+
+is_installed(){
+    [[ -f /usr/local/lib/${LIBNAME}.a ]]
+}
+
+make_and_install(){
+    pushd "${SCRIPTDIR}/../${LIBNAME}" > /dev/null
+
+    ./autogen.sh
+    ./configure
+    make
+    sudo make install
+
+    popd > /dev/null
+}
+
+main(){
+    if is_installed; then
+        echo 'Already installed'
+        exit 0
+    fi
+
+    make_and_install
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    # Bash Strict Mode
+    set -eu -o pipefail
+
+    # set -x
+    main "$@"
+fi
+
+
